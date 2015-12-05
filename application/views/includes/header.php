d7c0dcfd74b9d25a93afe90898d7d98deb93dbf4
Jayadeep KM <kmjayadeep@gmail.com>
2015-12-05 18:35:10 +0530

    initial

---
+++ application/views/includes/header.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>IEEE Computer Science India Symposium</title>
+    <meta name="description"
+          content="Accomodation is developed for hotels, motels, guest house and accommodation planning like Vacation Rentals, Homes, Apartments & Rooms and much more.">
+    <meta name="author" content="Themeinjection.com">
+
+    <!-- Bootstrap -->
+    <link href="<?=base_url('assets')?>/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Bootstrap 3 Date Picker -->
+    <link href="<?=base_url('assets/')?>/bower_components/bootstrap-3-datepicker/dist/css/bootstrap-datepicker.min.css" rel='stylesheet' type='text/css'>
+
+    <!-- Google Open Sans Font -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
+
+    <!-- Font Awesome -->
+    <link href="<?=base_url('assets/')?>/bower_components/font-awesome/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+
+    <!-- Animate Css -->
+    <link href="<?=base_url('assets/')?>/bower_components/animate.css/animate.min.css" rel='stylesheet' type='text/css'>
+
+    <!-- Simple Line Icons -->
+    <link href="<?=base_url('assets/')?>/bower_components/simple-line-icons/css/simple-line-icons.css" rel='stylesheet' type='text/css'>
+
+
+    <!-- Theme Style -->
+    <link rel="stylesheet" href="<?=base_url('assets/')?>/css/styles.css">
+    <!-- Available main styles: styles-blue.css, styles-ochre.css -->
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?=base_url('assets/')?>/img/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?=base_url('assets/')?>/img/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?=base_url('assets/')?>/img/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="<?=base_url('assets/')?>/img/ico/apple-touch-icon-57-precomposed.png">
+    <link rel="shortcut icon" href="<?=base_url('assets/')?>/img/ico/favicon.png">
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="<?=base_url('assets/')?>/bower_components/jquery/dist/jquery.min.js"></script>
+
+<script src="<?=base_url('assets/')?>/bower_components/jquery-placeholder/jquery.placeholder.min.js"></script>
+<script src="<?=base_url('assets/')?>/bower_components/waypoints/lib/jquery.waypoints.min.js"></script>
+<script src="<?=base_url('assets/')?>/bower_components/jquery-animatenumber/jquery.animateNumber.min.js"></script>
+<script src="<?=base_url('assets/')?>/bower_components/bootstrap-3-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="<?=base_url('assets/')?>/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+<!-- Google Maps Api -->
+<script style="" type="text/javascript" src="http://maps.google.com/maps/api/js"></script>
+
+
+<script src="<?=base_url('assets/')?>/js/custom.js"></script>
---
+++ application/views/registration.php
@@ -0,0 +1,216 @@
+
+</head>
+<body>
+<div id="page-top"></div>
+
+<!--Navigation Top start-->
+<section class="top-navbar container navbar-fixed-top">
+    <nav class="navbar navbar-default" id="navigation-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!--Brand / Logo start-->
+                <a class="navbar-brand scroll-to" href="#page-top">
+                    <img src="<?=base_url('assets/')?>/ieee/ieee2.gif" class="img-responsive" alt="Accommodation Landing Page"/>
+                </a>
+                <!--Brand / Logo end-->
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <!-- Nav-Links start -->
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a class="scroll-to" href="<?=base_url()?>">HOME</a></li>
+                    <li><a class="scroll-to" href="#sc-reviews">SPEAKERS</a></li>
+                    <li class="active"><a href="<?=base_url('registration')?>">REGISTRATION</a></li>
+                    <li><a class="scroll-to" href="#sc-location">WORKSHOPS</a></li>
+                    <li><a href="<?=base_url('registration')?>">HOSPITALITY</a></li>
+                    <li><a class="scroll-to" href="#sc-contactus">CONTACT US</a></li>
+                    <li>
+                      
+                    </li>
+                </ul>
+                <!-- Nav-Links end -->
+            </div>
+        </div>
+    </nav>
+</section>
+<!--Navigation Top end-->
+
+<!--Teaser Slider end-->
+
+<!--Start content before Slider-->
+<div class="content-after-slider">
+
+    <!--Attractions start-->
+    <section class="attractions" id="sc-attractions" style="margin-top:6em">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h2  class="head1">REGISTRATION</h2>
+ <section class="contact" id="sc-contact">
+        <div class="container">
+            <form action="#" id="contact-form">
+                <input type="hidden" name="action" value="send_contact_form"/>
+
+                <div class="alert hidden" id="contact-form-msg"></div>
+                <div class="row">
+                    <div class="col-md-6 col-md-offset-3">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-user fa-fw"></i></span>
+                            <input type="text" name="name" class="form-control" placeholder="Name">
+                        </div>
+                        <br>
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-home fa-fw"></i></span>
+                            <input type="text" name="mid" class="form-control" placeholder="Membership Id">
+                        </div>
+                        <br>
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-home fa-fw"></i></span>
+                          <select class="form-control">
+                            	<option disabled="disabled" selected="selected">Section</option>
+                            	<option>Kerala</option>
+                            	<option>Madras</option>
+                            </select>
+                        </div>
+                        <br>
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-home fa-fw"></i></span>
+                            <input type="text" name="email" class="form-control" placeholder="College">
+                        </div>
+                        <br>
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-home fa-fw"></i></span>
+                            <select class="form-control">
+                            	<option disabled="disabled" selected="selected">Food Preference</option>
+                            	<option>Veg</option>
+                            	<option>Non Veg</option>
+                            </select>
+                        </div>
+                        <br>
+                        <h2>Accomodation</h2>
+                         <div class="input-group input-inline">
+                        <input type="checkbox" class="form-control">26-02-2016
+                        </div>
+                         <div class="input-group">
+                        <input type="checkbox" class="form-control">27-02-2016
+                        </div>
+                         <div class="input-group">
+                        <input type="checkbox" class="form-control">28-02-2016
+                        </div>
+                        <br>
+                                 <p class="contact-form-info">*All fields are required.</p>
+                        <button class="btn btn-color1 btn-contact-form">Submit Form <i class="fa fa-angle-right"></i></button>
+                    </div>
+                    
+                </div>
+                
+            </form>
+        </div>
+    </section>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <!--Attractions end-->
+
+
+    <!--Contact start-->
+    <section class="contact" id="sc-contactus">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="contact-header">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <h2>Get in touch with Us</h2>
+
+                                <h3>If you have any questions feel free to contact us!</h3>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="contact-person">EVENT COORDINATORS</div>
+                                <br>
+                                <div class="contact-img">
+                                    <img class="img-responsive" src="<?=base_url('assets/img/torso.png')?>" alt="Contact Image"/>
+                                </div>
+                                
+                                <div class="contact-title">SANJU SAJEEV</div>
+                                <div class="contact-phone"><i class="fa fa-mobile"></i>+91 9020057354</div>
+                                <br><br><br>
+                                <div class="contact-img">
+                                    <img class="img-responsive" src="<?=base_url('assets/img/torso.png')?>" alt="Contact Image"/>
+                                </div>
+                                
+                                <div class="contact-title">KM JAYADEEP</div>
+                                <div class="contact-phone"><i class="fa fa-mobile"></i>+91 9567125807</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <!--Contact end-->
+
+    <!--Address start-->
+    <section class="address" id="sc-address">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--Address end-->
+
+    <!--Footer start-->
+    <footer class="footer" id="sc-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <p class="footer-copyright">
+                        &copy;  All rights reserved.
+                    </p>
+                </div>
+                <div class="col-md-6">
+                    <ul class="footer-social-media">
+                        <li><a href="#"><i class="fa fa-twitter fa-fw"></i></a></li>
+                        <li><a href="#"><i class="fa fa-google-plus fa-fw"></i></a></li>
+                        <li><a href="https://www.facebook.com/ieeecsis/"><i class="fa fa-facebook fa-fw"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--Footer end-->
+</div>
+<!--End content before Slider-->
+
+<a href="#page-top" class="scroll-to scroll-up-btn"><i class="fa fa-angle-up"></i></a>
+
+<!--Room Tabs Gallery Preview Modal start-->
+<div class="modal fade" id="roomTabsGalleryPreviewModal" tabindex="-1" role="dialog" aria-labelledby="roomTabsGalleryPreviewModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-nav">
+                <div class="title pull-left"><!-- title via js --></div>
+                <button type="button" class="close pull-right" data-dismiss="modal"><span
+                        aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+            </div>
+            <div class="modal-body" id="roomTabsGalleryPreviewModalLabel">
+                <!-- img via js -->
+            </div>
+        </div>
+    </div>
+</div>
+<!--Room Tabs Gallery Preview Modal start-->
+
