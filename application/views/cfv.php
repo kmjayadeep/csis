f3e660d2c4be5f49baedca9799acb235ede5f315
kevin joseph <josephkevin1995@gmail.com>
2015-12-10 14:24:25 +0530

    cfv

---
+++ application/views/cfv.php
@@ -0,0 +1,187 @@
+</head>
+<body>
+
+<!--Navigation Top start-->
+<section class="top-navbar container navbar-fixed-top" style="width:90%">
+    <nav class="navbar navbar-default" id="navigation-top">
+        <div class="container-fluid" style="background:white">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!--Brand / Logo start-->
+                <a class="navbar-brand scroll-to" href="#page-top" style="height: 71px;padding-top: 0px;margin-top: 0px;padding-bottom:0px;">
+                    <img src="<?=base_url('assets')?>/ieee/logo.png" class="img-responsive" alt="Accommodation Landing Page" style="width:260px"/>
+                </a>
+                <!--Brand / Logo end-->
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <!-- Nav-Links start -->
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="active"><a href="<?=base_url()?>">HOME</a></li>
+                    <li>
+                      
+                    </li>
+                </ul>
+                <!-- Nav-Links end -->
+            </div>
+        </div>
+    </nav>
+</section>
+<!--Navigation Top end-->
+
+<!--Teaser Slider end-->
+
+<!--Start content before Slider-->
+<div class="content-after-slider">
+
+    <!--Attractions start-->
+    <section class="attractions" id="sc-attractions" style="margin-top:6em">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h2  class="head1">Volunteer Call</h2>
+ <section class="contact" id="sc-contact">
+        <div class="container">
+            <h2 style="margin-top:0px"></h2><br/>
+            <form action="#" id="contact-form">
+                <input type="hidden" name="action" value="send_contact_form"/>
+
+                <div class="alert hidden" id="contact-form-msg"></div>
+                <div class="row">
+                    <div class="col-md-6 col-md-offset-3">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-user fa-fw"></i></span>
+                            <input type="text" name="name" class="form-control" placeholder="Name">
+                        </div>
+                        <br>
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-barcode fa-fw"></i></span>
+                            <input type="text" name="mid" class="form-control" placeholder="Membership Id">
+                        </div>
+                        <br>
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-user fa-fw"></i></span>
+                          <select style="color: #57BCCA"class="form-control">
+                                <option disabled="disabled" selected="selected">Gender</option>
+                                <option>Male</option>
+                                <option>Female </option>
+                            </select>
+                        </div>
+                        <br>
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-phone fa-fw"></i></span>
+                            <input type="number" name="phone" class="form-control" placeholder="Phone No">
+                        </div>
+                        <br>
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-envelope fa-fw"></i></span>
+                            <input type="email" name="email" class="form-control" placeholder="Email Id">
+                        </div>
+                        <br>
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-home fa-fw"></i></span>
+                          <select style="color: #57BCCA" class="form-control">
+                            	<option disabled="disabled" selected="selected">Section</option>
+                                <option>Banglore</option>
+                                <option>Bombay</option>
+                                <option>Kolkata</option>
+                                <option>Delhi</option>
+                                <option>Gujrat</option>
+                                <option>Hyderabad</option>
+                                <option>Kerala</option>
+                                <option>Kharagpur</option>
+                                <option>Madras</option>
+                                <option>Pune</option>
+                                <option>Uttar Pradesh</option>
+                            </select>
+                        </div>
+                        <br>
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-user fa-fw"></i></span>
+                          <select style="color: #57BCCA"class="form-control">
+                                <option disabled="disabled" selected="selected">Are you a CS member?</option>
+                                <option>Yes</option>
+                                <option>No</option>
+                            </select>
+                        </div>
+                        <br>
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-book fa-fw"></i></span>
+                            <input type="text" name="email" class="form-control" placeholder="College">
+                        </div>
+                        <br>
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-user fa-fw"></i></span>
+                          <select style="color: #57BCCA"class="form-control">
+                                <option disabled="disabled" selected="selected">Are you ready to join discussion in WhatsApp?</option>
+                                <option>Yes</option>
+                                <option>No</option>
+                            </select>
+                        </div>
+                        <br>
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-clock-o fa-fw"></i></span>
+                            <input type="number" name="hrs" class="form-control" placeholder="How much time can you spend for CSIS daily?">
+                        </div>
+                        <br>
+                            <textarea placeholder="Previous volunteering experiences"form="contact" name="content"cols="10" rows="5" class="form-control" style=" color:#57BCCA; background-color: #f1f1f1;border-bottom-width: 1px;padding-bottom: 20px;margin-bottom: 13px;"></textarea>
+                        <br>
+                        <h4>Which field would you like to volunteer in?</h4>
+
+
+                        <div class="acco">
+
+                         <div class="input-group">
+                        <input type="checkbox" class="form-control">Design
+                        </div>
+                         <div class="input-group">
+                        <input type="checkbox" class="form-control">Publicity
+                        </div>
+
+                        </div>
+                                 <p class="contact-form-info">*All fields are required.</p>
+                        <button class="btn btn-color1 btn-contact-form">Proceed to payment<i class="fa fa-angle-right"></i></button>
+                    </div>
+                    
+                </div>
+                
+            </form>
+        </div>
+    </section>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <!--Attractions end-->
+
+
+    <!--Footer start-->
+    <footer class="footer" id="sc-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <p class="footer-copyright">
+                        &copy;  IEEE CET Computer Society Chapter Web Team.
+                    </p>
+                </div>
+                <div class="col-md-6">
+                    <ul class="footer-social-media">
+                        <li><a target="_blank" href="https://www.facebook.com/ieeecsis/"><i class="fa fa-facebook fa-fw"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--Footer end-->
+</div>
+<!--End content before Slider-->
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="<?=base_url('assets/')?>/bower_components/jquery/dist/jquery.min.js"></script>
+<script src="<?=base_url('assets/')?>/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
