0be1d796b4cee0990006331f77609a3c71f4da92
Jayadeep KM <kmjayadeep@gmail.com>
2016-01-12 07:39:12 +0530

    payment gateway callback api

--- application/controllers/Paysuccess.php
+++ application/controllers/Paysuccess.php
@@ -9,4 +9,20 @@ class Paysuccess extends CI_Controller {
 		$this->load->view('paysuccess');
 		$this->load->view('includes/footer');
 	}
+
+	public function api(){
+		$d = $this->input->post('data');
+		$data = json_decode($d);
+		if(is_array($data))
+			$data = $data[0];
+
+		$email =  $data->userEmailId;
+
+		$this->db->update('users',array(
+				'payment_status'=>1
+			),array(
+				'email'=>$email
+			));
+		echo 'done';
+	}
 }
