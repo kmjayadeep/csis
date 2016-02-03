5cd2dcfd16d1755b086dbdf17a627f58e98459ef
Jayadeep KM <kmjayadeep@gmail.com>
2016-02-03 13:39:58 +0530

    phase 2 link redirect

---
+++ application/controllers/Regnoncs2.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Regnoncs2 extends CI_Controller {
+
+	public function index()
+	{
+
+		redirect('/regnoncs/p2');
+	}
+
+
+}
